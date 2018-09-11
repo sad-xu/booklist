8fb1f52023dbad688af41febcc05e786d2c11c53
xhc <1031568754@qq.com>
2018-09-11 22:42:49 +0800

    start do web...

---
+++ web/src/components/analyse/analyse.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="analyse-wrapper">
+		<span>{{msg}}</span>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'analyse',
+		data() {
+			return {
+				msg: 'ok0'
+			}
+		},
+		created() {
+
+		},
+	}
+</script>
+
+<style scoped type="text/scss" lang="scss">
+	.analyse-wrapper {
+		background-color: red;
+	}
+</style>
---
+++ web/src/components/home.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="all">
+		<span>{{msg}}</span>
+		<router-link to="/analyse">link to...</router-link>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'home',
+		data() {
+			return {
+				msg: 'ok'
+			}
+		},
+		created() {
+			console.log('oooo')
+		},
+	}
+</script>
+
+<style scoped lang="scss"  type="text/scss" >
+	.all {
+		background-color: green;
+	}
+</style>
